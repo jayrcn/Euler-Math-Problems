e389a235d7b0602bb9f7c1bfe90e41802024232f
Jay! <90228449+jayrcn@users.noreply.github.com>
2023-06-09 17:26:25 -0700

    Delete Problem 3 (Largest Prime Factors).py

--- Euler Coding Problems (Python)/Problem 3 (Largest Prime Factors).py
+++
@@ -1,6 +0,0 @@
-from sympy.ntheory import primefactors
-primefactors(600851475143)
-max(primefactors(600851475143))
-
-#Used a libary to figure this one out (know how to use libaries LOL they are useful)
-#For libaraies use juptyer notbooks (this was ran in J Notebooks)
