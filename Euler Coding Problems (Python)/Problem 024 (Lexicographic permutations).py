b9b1dfb1afbfe1de565e447168783c895b6aba12
James Ruel Niere (Jay) <90228449+jayrcn@users.noreply.github.com>
2023-06-14 15:42:03 -0700

    Update Problem 024 (Lexicographic permutations).py

--- Euler Coding Problems (Python)/Problem 024 (Lexicographic permutations).py
+++ Euler Coding Problems (Python)/Problem 024 (Lexicographic permutations).py
@@ -1,4 +1,4 @@
 import itertools
 def main():
-    print(list(itertools.permutations(range(10)))[999999])
+    print(list(itertools.permutations(range(10)))[999999]) #index starts at 0, so the 999,999 is the 1 millonth element
 main()
